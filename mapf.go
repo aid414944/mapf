c5306ac944547774f0f4ddd2d28dda27aeb6bece
aid414944 <aid414944@gmail.com>
2014-01-19 14:40:47 +0800

    Create mapf.go
    go

---
+++ mapf.go
@@ -0,0 +1,87 @@
+package mapf
+
+import (
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
+type Mapf struct {
+	filePath string
+	writeMode int
+	data map[string]interface{}
+}
+
+const (
+	MODE_AUTOWRITE = iota
+	MODE_NONWRITE
+)
+
+func New(path string)(m *Mapf, e error){
+
+	f, oe := os.OpenFile(path, os.O_RDWR, os.ModePerm)	
+	defer f.Close()
+
+	if oe != nil {
+		fmt.Printf("%v", oe)	
+		return nil, oe
+	}
+	
+	fileinfo, _ := f.Stat()
+	jsontext := make([]byte, fileinfo.Size())
+	if _ , re := f.Read(jsontext); re != nil {
+		fmt.Printf("%v", re)	
+		return nil, re
+	}
+
+	m = &Mapf{path, MODE_AUTOWRITE, make(map[string]interface{})}
+
+	if de := json.Unmarshal(jsontext, &m.data); de != nil {
+		fmt.Printf("%v", de)
+	}
+	
+	return
+}
+
+func (m *Mapf)Flush()(e error){
+
+	b, ee := json.Marshal(m.data)
+	if ee != nil{
+		fmt.Printf("%v", ee)
+		return ee
+	}
+
+	f, oe := os.OpenFile(m.filePath, os.O_RDWR, os.ModePerm)
+	if oe != nil{
+		fmt.Printf("%v", oe)	
+		return oe
+	}
+
+	defer f.Close()
+
+	_, we := f.Write(b)
+	if we != nil{
+		
+		fmt.Printf("%v",  we)
+		return we
+	}
+	
+	return nil
+}
+
+func (m *Mapf)SwitchMode(mode int){
+	m.writeMode = mode
+}
+
+func (m *Mapf)Put(k string, v interface{}) error {
+	m.data[k] = v
+	if m.writeMode == MODE_AUTOWRITE{
+		return m.Flush()
+	}
+	return nil
+}
+
+func (m *Mapf)Get(k string)(v interface{}, ok bool){
+	v, ok = m.data[k]
+	return 
+}
